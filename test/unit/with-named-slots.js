e37f3e1e4e04181b25ef66ce3590421422574c03
Nadia Makarevich <nmakarevich@atlassian.com>
2016-06-14 11:54:38 +1000

    fix: fix test and remove console.log

--- test/unit/with-named-slots.js
+++ test/unit/with-named-slots.js
@@ -18,30 +18,29 @@ describe('namded slots', function () {
 
     const hello = define('x-hello', {
       render() {
-        console.log('x-hello render');
         vdom.div({}, () => {
-          console.log('div render');
           vdom.create('atlas-animation', {}, () => {
-            console.log('atlas-animation render', hello);
             vdom.div({}, 'Bounce');
           });
         });
       },
     });
 
+
     const helloEleme = new hello();
 
     const ch2 = helloEleme[symbols.shadowRoot].firstElementChild;
 
     afterMutations(() => {
-      expect(ch2.innerHTML).to.equal("<atlas-animation defined=\"\"><div>Bounce</div></atlas-animation>");
+      expect(["<atlas-animation defined=\"\"><div>Bounce</div></atlas-animation>", "<atlas-animation defined><div>Bounce</div></atlas-animation>"].indexOf(ch2.innerHTML)>-1).to.equal(true);
       expect(ch2.textContent).to.equal("Bounce");
       expect(ch2.textContent).to.equal(ch2.__textContent);
 
       expect(ch2.__innerHTML).not.to.equal(`<atlas-animation defined=\"\"><${symbols.shadowRoot}><div><content></content></div></${symbols.shadowRoot}></atlas-animation>`);
+      expect(ch2.__innerHTML).not.to.equal(`<atlas-animation defined><${symbols.shadowRoot}><div><content></content></div></${symbols.shadowRoot}></atlas-animation>`);
 
       // not sure about this one, this is how it looks in browser without namedslots
-      expect(ch2.__innerHTML).to.equal(`<atlas-animation defined=\"\"><${symbols.shadowRoot}><div><content></content></div><div>Bounce</div></${symbols.shadowRoot}></atlas-animation>`);
+      expect([`<atlas-animation defined><${symbols.shadowRoot}><div><content></content></div><div>Bounce</div></${symbols.shadowRoot}></atlas-animation>`, `<atlas-animation defined=\"\"><${symbols.shadowRoot}><div><content></content></div><div>Bounce</div></${symbols.shadowRoot}></atlas-animation>`].indexOf(ch2.__innerHTML)>-1).to.equal(true);
     });
 
   })
